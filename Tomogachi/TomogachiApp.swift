db5ed96d9c131379738b58688eeecad62c085963
Aayan Asif <asifaayan76@gmail.com>
2026-01-10 12:25:02 -0500

    Initial Commit

---
+++ Tomogachi/TomogachiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomogachiApp.swift
+//  Tomogachi
+//
+//  Created by Aayan Asif on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct TomogachiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TomogachiTests/TomogachiTests.swift
@@ -0,0 +1,17 @@
+//
+//  TomogachiTests.swift
+//  TomogachiTests
+//
+//  Created by Aayan Asif on 2026-01-10.
+//
+
+import Testing
+@testable import Tomogachi
+
+struct TomogachiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
